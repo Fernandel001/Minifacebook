a48d739907a48a00af158236f41bb36121a63e00
Sulpiceson <werindoissireal@gmail.com>
2026-03-06 16:53:33 +0100

    Error Fix, deleted the redirection before deploying the frontend

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -29,7 +29,7 @@ router.get('/google/callback',
     });
 
     // Redirige vers le frontend
-    res.redirect(`${process.env.CLIENT_URL}/feed`);
+   // res.redirect(`${process.env.CLIENT_URL}/feed`);
        res.json({
         message: "Authentification réussie",
         user: req.user
@@ -43,13 +43,13 @@ router.get('/failed', (req, res) => {
   res.status(401).json({ error: 'Échec de la connexion Google' });
 });
 
-// ─── DÉCONNEXION ─────────────────────────────────────────────────────────
+// ─── DÉCONNEXION ──────────────────
 router.get('/logout', (req, res) => {
   res.clearCookie('token');
   res.json({ message: 'Déconnecté avec succès' });
 });
 
-// ─── RÉCUPÉRER L'UTILISATEUR CONNECTÉ ────────────────────────────────────
+// ─── RÉCUPÉRER L'UTILISATEUR CONNECTÉ 
 router.get('/me', auth, (req, res) => {
   // Le middleware 'auth' a déjà vérifié le token et mis l'utilisateur dans req.user
   res.json(req.user);
